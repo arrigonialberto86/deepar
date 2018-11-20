fb38dd9af35e0305b6cb8c0c456e453b7d2a3b54
Arrigoni Alberto <alberto.arrigoni@pirelli.com>
2018-11-20 17:59:42 +0100

    adding tests

--- deepar/model/lstm.py
+++ deepar/model/lstm.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from deepar.model import NNModel
 from deepar.model.layers import GaussianLayer
 from keras.layers import Input, Dense, Input
@@ -9,7 +10,7 @@ logger = logging.getLogger('deepar')
 
 
 class DeepAR(NNModel):
-    def __init__(self, ts_obj, steps_per_epoch, epochs, loss='mse', optimizer='adam', with_nn_structure=None):
+    def __init__(self, ts_obj, steps_per_epoch=50, epochs=100, loss='mse', optimizer='adam', with_nn_structure=None):
         self.ts_obj = ts_obj
         self.inputs, self.z_sample = None, None
         self.steps_per_epoch = steps_per_epoch
@@ -20,7 +21,7 @@ class DeepAR(NNModel):
         if with_nn_structure:
             self.nn_structure = with_nn_structure
         else:
-            self.nn_structure = DeepAR.basic_structure()
+            self.nn_structure = DeepAR.basic_structure
 
     @staticmethod
     def basic_structure():
@@ -28,25 +29,32 @@ class DeepAR(NNModel):
         This is the method that needs to be patched when changing NN structure
         :return: inputs, z_sample ()
         """
-        inputs = Input(shape=(20, 1))
+        input_shape = (20, 1)
+        inputs = Input(shape=input_shape)
         x = LSTM(10, return_sequences=True, dropout=0.2)(inputs)
         x = Dense(10, activation='relu')(x)
         z_sample = GaussianLayer(1, name='main_output')(x)
-        return inputs, z_sample
+        return inputs, z_sample, input_shape
 
     def instantiate_and_fit(self, verbose=False):
-        inputs, z_sample = DeepAR.net_structure()
+        inputs, z_sample, input_shape = self.nn_structure()
         model = Model(inputs, z_sample)
         model.compile(loss=self.loss, optimizer=self.optimizer)
-        model.fit_generator(ts_generator(self.ts_obj),
+        model.fit_generator(ts_generator(self.ts_obj,
+                                         1,
+                                         input_shape[0]),
                             steps_per_epoch=self.steps_per_epoch,
                             epochs=self.epochs)
         if verbose:
             logger.debug('Model was successfully trained')
         self.keras_model = model
 
+    @property
+    def model(self):
+        return self.keras_model
 
-def ts_generator(ts_obj, batch_shape):
+
+def ts_generator(ts_obj, batch_shape, n_steps):
     """
     This is a util generator function for Keras
     :param ts_obj: a Dataset child class object that implements the 'next_batch' method
@@ -54,5 +62,5 @@ def ts_generator(ts_obj, batch_shape):
     :return:
     """
     while 1:
-        batch = ts_obj.next_batch(batch_shape)
+        batch = ts_obj.next_batch(batch_shape, n_steps)
         yield batch[0], batch[1]
---
+++ tests/test_dataset.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import unittest
+from deepar.dataset.time_series import TimeSeries
+
+
+class TestRecurrentTs(unittest.TestCase):
+    def setUp(self):
+
+        self.data_to_pad = pd.DataFrame({'feature_1': [i for i in range(6)],
+                                         'feature_2': [i for i in range(6)],
+                                         'target': [i for i in range(6)]})
+
+        self.input_data = pd.DataFrame({'feature_1': [i for i in range(100)],
+                                        'feature_2': [i for i in range(100)],
+                                        'target': [i for i in range(100)],
+                                        'category': [str(int(i//10 + 1)) for i in range(100)]})
+
+        self.data_to_pad_with_categorical = pd.DataFrame({'one_hot_yes': [1, 1, 1, 1, 1, 1],
+                                                          'feature_2': [i for i in range(6)],
+                                                          'one_hot_no': [0, 0, 0, 0, 0, 0],
+                                                          'target': [i for i in range(6)]})
+        self.data_to_pad_with_multiple_categorical = pd.DataFrame({'one_hot_yes': [1, 1, 1, 1, 1, 1],
+                                                                   'feature_2': [i for i in range(6)],
+                                                                   'one_hot_no': [0, 0, 0, 0, 0, 0],
+                                                                   'other_no': [0, 0, 0, 0, 0, 0],
+                                                                   'other_yes': [1, 1, 1, 1, 1, 1],
+                                                                   'target': [i for i in range(6)]})
+
+    def test_len_padding(self):
+        rec_instance = TimeSeries(pandas_df=self.data_to_pad)
+        results = rec_instance._pad_ts(pandas_df=self.data_to_pad,
+                                       desired_len=10)
+        self.assertEqual(results.shape[0], 10)
+
+    def test_zero_len_padding(self):
+        rec_instance = TimeSeries(pandas_df=self.data_to_pad)
+        results = rec_instance._pad_ts(pandas_df=self.data_to_pad,
+                                       desired_len=6)  # len is the same as the original time series
+        self.assertEqual(results.shape[0], 6)
+
+    def test_next_batch_production(self):
+        rec_ts = TimeSeries(self.input_data)
+        X_feature_space, y_target = rec_ts.next_batch(batch_size=4, n_steps=10)
+        self.assertEqual(len(X_feature_space), 4)
+        self.assertEqual(len(X_feature_space[0]), 10)
+        self.assertEqual(len(X_feature_space[0][0]), 2)
+        self.assertEqual(X_feature_space[3][0][0], y_target[3][0][0])
+
+    def test_padding_with_one_hot(self):
+        rec_ts = TimeSeries(pandas_df=self.data_to_pad_with_categorical,
+                            one_hot_root_list=['one_hot'])
+        results = rec_ts._pad_ts(pandas_df=self.data_to_pad_with_categorical,
+                                 desired_len=10)
+
+        self.assertEqual(results.shape[0], 10)
+        self.assertEqual(results.one_hot_yes.values[0], 1)
+        self.assertEqual(results.one_hot_no.values[0], 0)
+
+    def test_padding_with_one_hot_multiple(self):
+        rec_ts = TimeSeries(pandas_df=self.data_to_pad_with_categorical,
+                            one_hot_root_list=['one_hot', 'other'])
+
+        results = rec_ts._pad_ts(pandas_df=self.data_to_pad_with_multiple_categorical,
+                                 desired_len=10)
+
+        self.assertEqual(results.shape[0], 10)
+        self.assertEqual(results.one_hot_yes.values[0], 1)
+        self.assertEqual(results.one_hot_no.values[0], 0)
+        self.assertEqual(results.other_yes.values[0], 1)
+        self.assertEqual(results.other_no.values[0], 0)
+
+    def test_next_batch_covariates(self):
+        """
+        Feature space is supplied in input if target_only is False (no need to lag y dataset)
+        :return:
+        """
+        rec_ts = TimeSeries(self.input_data)
+        X_feature_space, y_target = rec_ts.next_batch(batch_size=1, n_steps=10)
+        self.assertEqual(len(X_feature_space), 1)
+        self.assertEqual(len(X_feature_space[0][0]), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
